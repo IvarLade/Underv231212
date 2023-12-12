86c6a4bee6a400448e528929a30aed69b91f2c2a
IvarLade <ivarlade@gmail.com>
2023-12-12 12:36:01 +0100

    Updates

--- 002/02-gen-carousel.py
+++ 002/02-gen-carousel.py
@@ -1,13 +1,13 @@
 import os
 
 def generate_html_page(page_number):
-    page_name = f"page{page_number:02d}"
+    page_name = f"side{page_number:02d}"
     image_name = f"{page_name}.png"
-    next_page = f"page{page_number + 1:02d}.html"
+    next_page = f"side{page_number + 1:02d}.html"
 
     html_content = f"""
     <!DOCTYPE html>
-    <html lang="en">
+    <html lang="no">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -25,9 +25,11 @@ def generate_html_page(page_number):
     </html>
     """
 
-    with open(f"{page_name}.html", "w") as file:
+    with open(f"Underv231212/002/{page_name}.html", "w") as file:
         file.write(html_content)
 
 # Create 24 HTML pages
-for page_number in range(1, 25):
+# for page_number in range(1, 25):
+# Create 12 HTML pages
+for page_number in range(1, 13):
     generate_html_page(page_number)
